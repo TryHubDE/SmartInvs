390ee6247b502c6230b6933634f826fe75f6fd20
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-03-18 12:50:02 +0100

    remove instance method in SmartInvsPlugin

--- src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
+++ src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
@@ -4,18 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SmartInvsPlugin extends JavaPlugin {
 
-    private static SmartInvsPlugin instance;
     private static InventoryManager invManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-
         invManager = new InventoryManager(this);
         invManager.init();
     }
 
     public static InventoryManager manager() { return invManager; }
-    public static SmartInvsPlugin instance() { return instance; }
 
 }
